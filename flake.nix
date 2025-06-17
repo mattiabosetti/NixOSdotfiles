0fcdac63adbf73498869929beb3b31efa46cfe1e
mattiabosetti <mattbos.2007@gmail.com>
2025-06-17 21:57:24 +0200

    Aggiunto home-manager

--- flake.nix
+++ flake.nix
@@ -1,21 +1,33 @@
 {
-  description = "My first flake";
+  description = "Home Manager configuration of mattiab";
 
   inputs = {
-    nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, ... }:
-  let
-    lib = nixpkgs.lib;
-  in {
-    nixosConfigurations = {
-      nixos = lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [ ./configuration.nix ]; 
+  outputs =
+    { self, nixpkgs, home-manager, ... }:
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      nixConfigurations = {
+        nixos = lib.nixosSystem {
+          inherit system;
+          modules = [ ./configuration.nix ];
+        };
       };
-    };
-  };
+      homeConfigurations = {
+        mattiab = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [ ./home.nix ];
+        };
+        }; 
+      };
 }
---
+++ home.nix
@@ -0,0 +1,45 @@
+{ config, pkgs, inputs, ... }:
+
+{
+
+  nixpkgs.config.allowUnfree = true;
+
+  home.username = "mattiab";
+  home.homeDirectory = "/home/mattiab";
+
+  home.stateVersion = "24.11"; 
+
+  home.packages = [
+    pkgs.kdePackages.kcalc
+    pkgs.kdePackages.kate
+    pkgs.kdePackages.discover
+    pkgs.vscode
+    pkgs.obs-studio
+    pkgs.fastfetch
+    pkgs.starship
+    pkgs.lutris
+    pkgs.wine
+    pkgs.wine-wayland
+    pkgs.lazygit
+    pkgs.obsidian
+    pkgs.steam
+    pkgs.vesktop
+  ];
+  programs.git = {
+    userName = "mattiabosetti";
+    userEmail = "mattbos.2007@gmail.com";
+  };
+
+  services.kdeconnect = {
+    enable = true;
+    package = pkgs.kdePackages.kdeconnect-kde;
+    indicator = true;
+  };
+
+  home.file = {
+  };
+  home.sessionVariables = {
+  };
+
+  programs.home-manager.enable = true;
+}
