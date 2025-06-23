54105d03b254d3cd0795cf50e7cfe14821f61209
installer <installer@gmail.com>
2025-06-23 12:03:04 +0200

    aggiunto supporto xwayland e cazzate varie

--- modules/xserver.nix
+++ modules/xserver.nix
@@ -1,5 +1,8 @@
 { config, pkgs, ... }: {
   services.xserver.enable = true;
+  services.xserver.videoDrivers = [ "intel" ];
+  hardware.opengl.enable = true;
+  programs.xwayland.enable = true;
   services.xserver.xkb = {
     layout = "it";
     variant = "";
--- programs/programs.nix
+++ programs/programs.nix
@@ -14,6 +14,7 @@
     firefox
     python314
     eza
+    mesa
   ];
     
   programs.neovim = {
