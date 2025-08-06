59e1b9830802b22eba7d39d7a8470bd9a5ad36dc
mattiabosetti <installer@gmail.com>
2025-08-06 20:02:31 +0200

    Update flake.nix

--- modules/xserver.nix
+++ modules/xserver.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }: {
   services.xserver.enable = true;
   services.xserver.videoDrivers = [ "intel" ];
-  hardware.opengl.enable = true;
+  hardware.graphics.enable = true;
   programs.xwayland.enable = true;
   services.xserver.xkb = {
     layout = "it";
