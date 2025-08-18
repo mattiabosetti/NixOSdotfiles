8f5a939e531c5657ecc24f7efe0451ed302c8ba2
mattiabosetti <installer@gmail.com>
2025-08-18 21:40:01 +0200

    Aggiunto pciutils

--- configuration.nix
+++ configuration.nix
@@ -19,7 +19,7 @@
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
   ];
   environment.systemPackages = with pkgs; [
-    lspci
+    pciutils
  ];
   system.stateVersion = "25.05"; # Did you read the comment?
 }
